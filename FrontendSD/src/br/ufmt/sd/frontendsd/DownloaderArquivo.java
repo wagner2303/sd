fb3233116b98207ca25f9f2fdff5bad271640bd6
Wagner <wagner2303@gmail.com>
2014-08-24 20:21:00 -0400

    Adicionado comunicação Frontend to ClientD.

--- FrontendSD/src/br/ufmt/sd/frontendsd/DownloaderArquivo.java
+++ FrontendSD/src/br/ufmt/sd/frontendsd/DownloaderArquivo.java
@@ -3,27 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.ufmt.sd.frontendsd;
 
 import br.ufmt.sd.serverws.ClienteD;
 import br.ufmt.sd.serverws.DescricaoArquivo;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.RandomAccessFile;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author baby
  */
-public class DownloaderArquivo {
+public class DownloaderArquivo implements Runnable {
+
     private File arquivo;
     private DescricaoArquivo descricaoArquivo;
     private String nomeOriginal;
     private ArrayList<ClienteD> clientesD;
     private Float porcentagemConcluida;
-    private boolean[] partesConcluidas; 
-    
-    
+    private boolean[] partesConcluidas;
 
     public DownloaderArquivo() {
     }
@@ -33,10 +38,11 @@ public class DownloaderArquivo {
         this.descricaoArquivo = descricaoArquivo;
         this.nomeOriginal = nomeOriginal;
         this.clientesD = clientesD;
-        this.partesConcluidas = new boolean[getDescricaoArquivo().getTamanho().intValue()];
+        this.partesConcluidas = new boolean[Long.valueOf(this.descricaoArquivo.getTamanho() / 1024 + 1).intValue()];
         for (int i = 0; i < partesConcluidas.length; i++) {
-            partesConcluidas[i] = false;            
+            partesConcluidas[i] = false;
         }
+
     }
 
     public File getArquivo() {
@@ -47,7 +53,7 @@ public class DownloaderArquivo {
         this.arquivo = arquivo;
     }
 
-    public DescricaoArquivo getDescricaoArquivo() {
+    public final DescricaoArquivo getDescricaoArquivo() {
         return descricaoArquivo;
     }
 
@@ -55,7 +61,7 @@ public class DownloaderArquivo {
         this.descricaoArquivo = descricaoArquivo;
         this.partesConcluidas = new boolean[getDescricaoArquivo().getTamanho().intValue()];
         for (int i = 0; i < partesConcluidas.length; i++) {
-            partesConcluidas[i] = false;            
+            partesConcluidas[i] = false;
         }
     }
 
@@ -82,6 +88,71 @@ public class DownloaderArquivo {
     public void setPorcentagemConcluida(Float porcentagemConcluida) {
         this.porcentagemConcluida = porcentagemConcluida;
     }
-        
-    
+
+    private boolean hasPartes() {
+        int i = 0;
+        while (i < partesConcluidas.length) {
+            if (partesConcluidas[i] == false) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
+    private int getFirstPart() {
+        int i = 0;
+        while (i < partesConcluidas.length) {
+            if (partesConcluidas[i] == false) {
+                return i;
+            }
+            i++;
+        }
+        return 0;
+    }
+
+    public void writeToFile(String filePath, byte[] data, int position)
+            throws IOException {
+
+        RandomAccessFile file = new RandomAccessFile(filePath, "rwd");
+        file.seek(position);
+//        file.write(data, 0, quantidade);
+        file.write(data);
+        file.close();
+
+    }
+
+    @Override
+    public void run() {
+        int conexoes = 0;
+        // TODO
+        Socket socket;
+        try {
+            socket = new Socket(getClientesD().get(0).getEndereco(), 5678);
+            PrintStream writer = new PrintStream(socket.getOutputStream());
+            InputStream reader = socket.getInputStream();
+            while (hasPartes()) {
+                // 192.168.0.5
+                writer.println(getDescricaoArquivo().getMd5Arquivo());
+                
+                int parte = getFirstPart();
+                writer.println(getFirstPart());
+                
+                System.out.print(parte + "\t");
+                byte[] buffer = new byte[1024];
+//                int bytes = 1024;
+//                if((parte + 1)* 1024 > descricaoArquivo.getTamanho()){
+//                    bytes = descricaoArquivo.getTamanho().intValue() - parte*1024;
+//                }
+                System.out.println(reader.read(buffer));
+                writeToFile(arquivo.getAbsolutePath(), buffer, parte * 1024);
+//                socket.close();
+                partesConcluidas[parte] = true;
+            }
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(DownloaderArquivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
--- FrontendSD/src/br/ufmt/sd/frontendsd/FXMLDocumentController.java
+++ FrontendSD/src/br/ufmt/sd/frontendsd/FXMLDocumentController.java
@@ -6,8 +6,10 @@
 package br.ufmt.sd.frontendsd;
 
 import br.ufmt.sd.frontendsd.model.ItemTabelaArquivos;
+import br.ufmt.sd.serverws.ClienteD;
 import br.ufmt.sd.serverws.DescricaoArquivo;
 import br.ufmt.sd.serverws.ItemBuscaNome;
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,17 +17,11 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.util.Callback;
 
 /**
  *
@@ -55,22 +51,6 @@ public class FXMLDocumentController implements Initializable {
 
         // Mostrar a lista de arquivos na tabela
         tableListaArquivos.setItems(observableList);
-
-        tableListaArquivos.setRowFactory(new Callback<TableView<ItemTabelaArquivos>, TableRow<ItemTabelaArquivos>>() {
-
-            @Override
-            public TableRow<ItemTabelaArquivos> call(TableView<ItemTabelaArquivos> p) {
-                final TableRow<ItemTabelaArquivos> row = new TableRow<>();
-                row.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent t) {
-                        showBottonPaneBusca(row.getItem());
-                    }
-                });
-
-                return row;
-            }
-        });
     }
 
 //    @FXML
@@ -83,16 +63,27 @@ public class FXMLDocumentController implements Initializable {
 //    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // TODOe
     }
 
-    private void showBottonPaneBusca(ItemTabelaArquivos ita) {
-     
-        HBox hBox = new HBox();
-        
-        Button btnOk = new Button("Baixar");
-        hBox.getChildren().addAll(btnOk);
-        vBoxBottonBusca.getChildren().add(hBox);
+    @FXML
+    private void handleButtonBaixar(ActionEvent event) {
+
+        ItemTabelaArquivos item = (ItemTabelaArquivos) tableListaArquivos.getSelectionModel().getSelectedItem();
+        if (item != null) {
+            System.out.println(item.getNomeArquivo());
+            DescricaoArquivo descricaoArquivo = new DescricaoArquivo();
+            descricaoArquivo.setMd5Arquivo(item.getMd5Arquivo());
+            descricaoArquivo.setTamanho(item.getTamanho());
+            ArrayList<ClienteD> clienteDs = new ArrayList<>();
+            for (ClienteD clienteD : getClientsD(descricaoArquivo)) {
+                clienteDs.add(clienteD);
+            }
+            DownloaderArquivo da = new DownloaderArquivo(
+                    new File("/home/baby/Área de Trabalho/arquivo"),
+                    descricaoArquivo, item.getNomeArquivo(), clienteDs);
+            da.run();
+        }
     }
 
     private static java.util.List<br.ufmt.sd.serverws.ItemBuscaNome> buscaArquivo(java.lang.String termo) {
@@ -101,10 +92,10 @@ public class FXMLDocumentController implements Initializable {
         for (int i = 1; i < 40; i++) {
             ItemBuscaNome itemBuscaNome = new ItemBuscaNome();
             DescricaoArquivo da = new DescricaoArquivo();
-            da.setTamanho(Long.valueOf(1000000000));
-            da.setMd5Arquivo("600996da6a7c25fd2b6614fac2aa440e");
+            da.setTamanho(new Long(113427l));
+            da.setMd5Arquivo("5c4de510acea159667f4fdac9ad5838b");
             itemBuscaNome.setDescricaoArquivo(da);
-            itemBuscaNome.setNomeArquivo("Xuxa só para baixinhos " + i);
+            itemBuscaNome.setNomeArquivo("gato " + i);
 
             itensBuscaNomes.add(itemBuscaNome);
         }
@@ -116,9 +107,16 @@ public class FXMLDocumentController implements Initializable {
     }
 
     private static java.util.List<br.ufmt.sd.serverws.ClienteD> getClientsD(br.ufmt.sd.serverws.DescricaoArquivo descricao) {
-        br.ufmt.sd.serverws.Server_Service service = new br.ufmt.sd.serverws.Server_Service();
-        br.ufmt.sd.serverws.Server port = service.getServerPort();
-        return port.getClientsD(descricao);
+        List<ClienteD> clienteDs = new ArrayList<>();
+        ClienteD clienteD = new ClienteD();
+        clienteD.setEndereco("192.168.0.5");
+        clienteD.setVelocidadeConexao(Float.parseFloat("10000"));
+
+        clienteDs.add(clienteD);
+        return clienteDs;
+//        br.ufmt.sd.serverws.Server_Service service = new br.ufmt.sd.serverws.Server_Service();
+//        br.ufmt.sd.serverws.Server port = service.getServerPort();
+//        return port.getClientsD(descricao);
     }
 
 }
